b945612a59e71661fbdf90138c03b9a36184f0c0
franciscojoaopedro <franciscojoaopedro1998@gmail.com>
2023-11-18 18:57:25 +0100

    fix remove collection and

--- src/models/postModel.js
+++ src/models/postModel.js
@@ -6,12 +6,7 @@ const postSchema = new mongoose.Schema({
     author: { type: mongoose.Schema.Types.ObjectId, ref: 'Author', required: true },
     comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
     createdAt: { type: Date, default: Date.now }
-},{
-    collation:"mednews"
-}
-
-
-);
+});
 
 const Post = mongoose.model('Post', postSchema);
 
